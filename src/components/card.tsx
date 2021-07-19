e405f83a17222481de5e94d4ca3d245244fc72c9
Babatunde Ojo <babatundeojo@MacBook-Pro.local>
2021-07-19 08:51:09 +0100

    revamp card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -26,13 +26,17 @@ const Card = (props: any) => {
                     </div>
                     <div className="tuition-container">
                         <span id="tuition-name">Est. Tuition</span>
-                        <span id="tuition-cost">{props?.data.currency} 73,800 / Year</span>
+                        <span id="tuition-cost">{props?.data.currency} {props?.data.estimated_tuition || props?.data.annual_tuition} / Year</span>
                     </div>
                     <hr/>
-                    <div className="course-container">
+                    {props.type == 'university' && <div className="course-container">
                         <span id="course-name">No. of courses</span>
-                        <span id="course-duration">32</span>
-                    </div>
+                        <span id="course-duration">{props?.data.number_of_courses}</span>
+                    </div>}
+                    {props.type == 'course' && <div className="course-container">
+                        <span id="course-name">Course Duration</span>
+                        <span id="course-duration">{`<1 Year`}</span>
+                    </div>}
                 </div>
                 <div id="button-container">
                     <button className="button gray-button" id="button1" >Learn More</button>
